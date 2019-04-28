beb5e2dcfaa7bae0a00c0c806275cc3fc69dd680
Aaron Leo Cooper <aaronleocooper@gmail.com>
2019-04-28 19:34:49 +0800

    feat: Added working template build script

--- create-tba-game/create.ts
+++ create-tba-game/create.ts
@@ -1,7 +1,55 @@
-interface Options {
-  name: string;
+import { readdirSync, existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
+
+import { version } from '../package.json';
+
+type Template = 'basic';
+
+export interface CreateOptions {
   title: string;
-  description: string;
+  about: string;
+  dirName: string;
+  template: Template;
 }
 
-export default function create(options: Options) {}
+export function getFilledTemplate(templateContent: string, replacements: string[][]): string {
+  return replacements.reduce(
+    (acc, [name, replacement]): string => acc.replace(new RegExp(`\\$${name}\\$`, 'g'), replacement),
+    templateContent
+  );
+}
+
+export default function create(options: CreateOptions): void {
+  const { title, about, dirName, template } = options;
+
+  const replacements = [
+    ['title', title],
+    ['about', about],
+    ['version', version]
+  ];
+
+  const targetPath = resolve(process.cwd(), dirName);
+
+  if (existsSync(targetPath)) {
+    throw new Error(`Target folder already exists, enter a new folder name`);
+  }
+
+  mkdirSync(targetPath);
+
+  const templatePath = resolve(__dirname, 'templates', template);
+
+  const templateFileNames = readdirSync(templatePath)
+    .filter((fileName): boolean => fileName.slice(-4) === '.txt');
+
+  templateFileNames.forEach((templateFileName): void => {
+    const templateFilePath = resolve(templatePath, templateFileName);
+    const templateContent = readFileSync(templateFilePath).toString();
+
+    const outFileName = templateFileName.slice(0, -4);
+    const outFilePath = resolve(targetPath, outFileName);
+
+    const outFileContent = getFilledTemplate(templateContent, replacements);
+
+    writeFileSync(outFilePath, outFileContent);
+  });
+}
--- create-tba-game/index.ts
+++ create-tba-game/index.ts
@@ -1,3 +1,69 @@
-const args = process.execArgv;
+import * as inquirer from 'inquirer';
+import { Question, Answers } from 'inquirer';
 
-console.info(args);
+import create from './create';
+import { CreateOptions } from './create';
+
+const trim = (str: string): string => str.trim();
+
+const getKebabCase = (str: string): string =>
+  trim(str).replace(/ /g, '-');
+
+const questions: Question[] = [
+  {
+    name: 'title',
+    message: 'Enter your game title',
+    type: 'input',
+    validate: (value: string): string | boolean => {
+      if (!trim(value)) {
+        return 'A game title is required';
+      }
+
+      return true;
+    }
+  },
+  {
+    name: 'about',
+    message: 'Enter a description (optional)',
+    type: 'input'
+  },
+  {
+    name: 'dirName',
+    message: 'Enter a name to use for the game folder',
+    type: 'input',
+    default: ({ title }: Answers): string => getKebabCase(title),
+    validate: (value: string): string | boolean => {
+      if (!trim(value)) {
+        return 'A folder name is required';
+      }
+
+      if (trim(value).includes(' ')) {
+        return 'Please use a folder name without spaces';
+      }
+
+      return true;
+    }
+  }
+];
+
+(async function runPrompt() {
+  console.log(
+    'Welcome to create-tba-game. This program will ' +
+    'create the basic setup required to start working ' +
+    'on your very own Text-Based Adventure game!\n' +
+
+    'You will be asked a few questions about your ' +
+    'desired game.\n'
+  );
+
+  const { title, about, dirName }: Answers = await inquirer.prompt(questions);
+
+  const options: CreateOptions = {
+    title: trim(title),
+    about: trim(about),
+    dirName: trim(dirName),
+    template: 'basic'
+  };
+
+  create(options);
+})();
